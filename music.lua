a71d657f15f6c199ecd169daaf3da97764a077ee
minidavid <njihiadavid02@gmail.com>
2026-03-10 04:42:50 +0300

    5. Will Add Music

--- music.lua
+++ music.lua
@@ -1,15 +1,17 @@
-
+--IMPORT MUSIC HERE
 function LoadSound()
     sound = love.audio.newSource("sfx.ogg","static")
     music = love.audio.newSource("sfx.ogg","stream")
 
 end
 
+--add music:play()/ music:pause()/ music:stop() logic here
 function music()
     music:play()
 
 end
 
+--add sfx:play()/ sfx:pause()/ sfx:stop() logic here
 function sfx()
     sfx:play()
 end
